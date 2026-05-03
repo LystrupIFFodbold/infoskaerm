30d85eb78bd79aa2a14524e1dff2eb7b65d02459
Kenneth Thomsen <kenneththomsen87@gmail.com>
2026-05-03 10:12:27 +0200

    Tilføj --ozone-platform=wayland til Chromium start-kommando

--- refresh_chromium.sh
+++ refresh_chromium.sh
@@ -4,6 +4,7 @@ export WAYLAND_DISPLAY=wayland-0
 pkill chromium 2>/dev/null
 sleep 3
 /usr/lib/chromium/chromium \
+  --ozone-platform=wayland \
   --kiosk --noerrdialogs --disable-infobars --no-first-run \
   --use-angle=gles --disable-dev-shm-usage --disable-features=Translate \
   file:///home/lif/infoskaerm/index.html > /dev/null 2>&1 &

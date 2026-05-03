7ab8539faad2178b47bb50b8da2f440901c6f6cd
Kenneth Thomsen <kenneththomsen87@gmail.com>
2026-05-03 10:09:12 +0200

    Sæt XDG_RUNTIME_DIR i refresh_chromium.sh så cron kan starte Chromium

--- refresh_chromium.sh
+++ refresh_chromium.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+export XDG_RUNTIME_DIR=/run/user/1000
+export WAYLAND_DISPLAY=wayland-0
 pkill chromium 2>/dev/null
 sleep 3
-WAYLAND_DISPLAY=wayland-0 /usr/lib/chromium/chromium \
+/usr/lib/chromium/chromium \
   --kiosk --noerrdialogs --disable-infobars --no-first-run \
   --use-angle=gles --disable-dev-shm-usage --disable-features=Translate \
   file:///home/lif/infoskaerm/index.html > /dev/null 2>&1 &
